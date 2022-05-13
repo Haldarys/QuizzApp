c09e8eba45e2bfbdae75287afbd9100eabce396f
Haldarys <jp45230@gmail.com>
2022-05-13 15:56:27 +0200

    Add login endpoint

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -7,9 +8,17 @@ def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
 
+@app.route('/login', methods=['POST'])
+def Login():
+    payload = request.get_json()
+    if(payload['password'] == 'mdp'):
+        return {"token": jwt_utils.build_token()}, 200
+    return '', 401
+
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
