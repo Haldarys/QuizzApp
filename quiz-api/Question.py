1f4ea6996f516b8776badebc63cdd755121481a2
Haldarys <jp45230@gmail.com>
2022-05-28 16:58:52 +0200

    Finish questions GET, POST, PUT, DELETE

---
+++ quiz-api/Question.py
@@ -0,0 +1,275 @@
+import json
+from multiprocessing.connection import Connection
+import sqlite3
+
+class Question():
+
+    def __init__(self, title: str, text: str, image: str, position: int, possibleAnswers: list, index: int = -1):
+        self.title = title
+        self.text = text
+        self.image = image
+        self.position = position
+        self.index = index
+        # possibleAnswer : [ {}, {}, ... ]
+        # keys : { id, question_id, text, isCorrect }
+        # values : { int, int, str, bool }
+        for answer in possibleAnswers:
+            answer['isCorrect'] = True if answer['isCorrect'] == 1 else False
+            
+        self.possibleAnswers = possibleAnswers
+
+    def __str__(self):
+        return self.toJson()
+
+    
+    def toJson(self):
+        return json.dumps(
+            {
+                "title": self.title,
+                "text": self.text,
+                "image": self.image,
+                "position": self.position,
+                "possibleAnswers": self.possibleAnswers
+            }
+        )
+    
+    @staticmethod
+    def fromJson(jsonInput: str):
+        input = json.loads(jsonInput)
+        return Question(input["title"], input["text"], input["image"], input["position"], input["possibleAnswers"])
+    
+    @staticmethod
+    def fromDict(dictInput: dict):
+        return Question(dictInput["title"], dictInput["text"], dictInput["image"], dictInput["position"], dictInput["possibleAnswers"])
+    
+    @staticmethod
+    def fromPosition(position: int):
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            db_conn.row_factory = sqlite3.Row
+            cur = db_conn.cursor()
+            cur.execute("begin")
+
+            # get question from db
+            select_result = cur.execute(
+                "SELECT id, text, title, image, position FROM Questions WHERE position = ?",
+                (position,)
+            )
+            res_question = select_result.fetchone()
+            if(res_question is None):
+                return False
+            id = res_question['id']
+
+            # get question answers from db
+            select_result = cur.execute(
+                "SELECT id, question_id, text, isCorrect FROM Answers WHERE question_id = ?",
+                (id,)
+            )
+            res_answer = select_result.fetchall()
+            possibleAnswers = []
+            for answer in res_answer:
+                answer_dict = dict(answer)
+                possibleAnswers.append(answer_dict)
+
+
+            cur.execute("commit")
+        except sqlite3.Error:
+            #in case of exception, roolback the transaction
+            cur.execute('rollback')
+            return False
+        
+        return Question(res_question["title"], res_question["text"], res_question["image"], res_question["position"], possibleAnswers, id)
+    
+    @staticmethod
+    def saveToDb(qst):
+        if qst.index >= 1:
+            return Question.updateIntoDb(qst)
+        else:
+            return Question.insertIntoDb(qst)
+
+    @staticmethod
+    def insertIntoDb(qst):
+        if not Question.incrementQuestionsOnInsert(qst):
+            return False
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            cur = db_conn.cursor()
+            cur.execute("begin")
+
+            # save the question to db
+            insertion_result = cur.execute(
+                "insert into Questions (title, text, image, position) values (?, ?, ?, ?)",
+                (qst.title, qst.text, qst.image, qst.position)
+            )
+            id = insertion_result.lastrowid
+
+            for answer in qst.possibleAnswers:
+                cur.execute(
+                    "insert into Answers (question_id, text, isCorrect) values (?, ?, ?)",
+                    (id, answer['text'],  1 if answer['isCorrect'] == True else 0)
+                )
+
+            cur.execute("commit")
+        except sqlite3.Error as err:
+            #in case of exception, rollback the transaction
+            print(err)
+            cur.execute('rollback')
+            db_conn.close()
+            return False
+        db_conn.close()
+        return True
+    
+    @staticmethod
+    def incrementQuestionsOnInsert(qst):
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            cur = db_conn.cursor()
+
+            cur.execute(
+                "UPDATE Questions SET position = position + 1 WHERE position >= ?",
+                (qst.position,)
+            )
+
+        except sqlite3.Error:
+            db_conn.close()
+            return False
+            
+        db_conn.close()
+        return True
+
+    @staticmethod
+    def decrementQuestionsOnDelete(qst):
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            cur = db_conn.cursor()
+
+            cur.execute(
+                "UPDATE Questions SET position = position - 1 WHERE position >= ?",
+                (qst.position,)
+            )
+
+        except sqlite3.Error:
+            db_conn.close()
+            return False
+            
+        db_conn.close()
+        return True
+
+    
+    @staticmethod
+    def updateIntoDb(qst):
+        if not Question.repositionQuestionsOnUpdate(qst):
+            return False
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            cur = db_conn.cursor()
+            cur.execute("begin")
+
+            # update the question
+            cur.execute(
+                "UPDATE Questions SET title = ?, text = ?, image = ?, position = ? WHERE id = ?",
+                (qst.title, qst.text, qst.image, qst.position, qst.index)
+            )
+
+            # delete old answers
+            cur.execute(
+                "DELETE FROM Answers WHERE question_id = ?",
+                (qst.index,)
+            )
+
+            for answer in qst.possibleAnswers:
+                cur.execute(
+                    "insert into Answers (question_id, text, isCorrect) values (?, ?, ?)",
+                    (qst.index, answer['text'],  1 if answer['isCorrect'] == True else 0)
+                )
+
+            cur.execute("commit")
+        except sqlite3.Error as err:
+            #in case of exception, rollback the transaction
+            print(err)
+            cur.execute('rollback')
+            db_conn.close()
+            return False
+        db_conn.close()
+        return True
+    
+    @staticmethod
+    def repositionQuestionsOnUpdate(qst):
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            db_conn.row_factory = sqlite3.Row
+            cur = db_conn.cursor()
+            cur.execute('begin')
+
+            position_result = cur.execute(
+                "SELECT position FROM Questions WHERE id = ?",
+                (qst.index,)
+            )
+            line = position_result.fetchone()
+            if(line is not None):
+                oldpos = line['position']
+            else:
+                return False
+
+            if oldpos > qst.position:
+                cur.execute(
+                    "UPDATE Questions SET position = position + 1 WHERE position <= ? AND position >= ? AND id <> ?",
+                    (oldpos, qst.position, qst.index)
+                )
+            else:
+                cur.execute(
+                    "UPDATE Questions SET position = position - 1 WHERE position >= ? AND position <= ? AND id <> ?",
+                    (oldpos, qst.position, qst.index)
+                )
+
+            cur.execute('commit')
+
+        except sqlite3.Error as err:
+            print(err)
+            cur.execute('rollback')
+            db_conn.close()
+            return False
+        
+        db_conn.close()
+        return True
+    
+    @staticmethod
+    def deleteQuestion(qst):
+        if qst.index <= 0:
+            return False
+        
+        if not Question.decrementQuestionsOnDelete(qst):
+            return False
+        
+        try:
+            db_conn = sqlite3.connect("./quiz.db")
+            db_conn.isolation_level = None
+            cur = db_conn.cursor()
+            cur.execute('begin')
+
+            cur.execute(
+                "DELETE FROM Questions WHERE id = ?",
+                (qst.index,)
+            )
+
+            cur.execute(
+                "DELETE FROM Answers WHERE question_id = ?",
+                (qst.index,)
+            )
+
+            cur.execute('commit')
+
+        except sqlite3.Error as err:
+            print(err)
+            cur.execute('rollback')
+            db_conn.close()
+            return False
+        
+        db_conn.close()
+        return True
--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,7 +1,21 @@
 from flask import Flask, request
+from lib import *
+from Question import Question
 import jwt_utils
+import sqlite3
 
 app = Flask(__name__)
+
+def authent():
+    jwt = request.headers.get('Authorization')
+    if(jwt):
+        jwt = jwt.split(' ')[1]
+    
+    try:
+        jwt_utils.decode_token(jwt)
+    except jwt_utils.JwtError:
+        return False
+    return True
 
 @app.route('/')
 def hello_world():
@@ -9,7 +23,7 @@ def hello_world():
 	return f"Hello, {x}"
 
 @app.route('/login', methods=['POST'])
-def Login():
+def PostLogin():
     payload = request.get_json()
     if(payload['password'] == 'mdp'):
         return {"token": jwt_utils.build_token()}, 200
@@ -19,6 +33,56 @@ def Login():
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
 
+@app.route('/questions', methods=['POST'])
+def PostQuestion():
+    if(not authent()):
+        return '', 401
+
+    payload = request.get_json()
+    qst = Question.fromDict(payload)
+    if not Question.saveToDb(qst):
+        return '', 500
+
+    return '', 200
+
+@app.route('/questions/<position>', methods=['GET'])
+def GetQuestion(position):
+    qst = Question.fromPosition(position)
+    if(not qst):
+        return '', 404
+
+    return qst.toJson(), 200
+
+@app.route('/questions/<position>', methods=['DELETE'])
+def DeleteQuestion(position):
+    if(not authent()):
+        return '', 401
+    
+    qst = Question.fromPosition(position)
+    if(not qst):
+        return '', 404
+
+    if not Question.deleteQuestion(qst):
+        return '', 500
+    return '', 204
+
+@app.route('/questions/<position>', methods=['PUT'])
+def UpdateQuestion(position):
+    if(not authent()):
+        return '', 401
+    
+    payload = request.get_json()
+    qst_new = Question.fromDict(payload)
+    qst_old = Question.fromPosition(position)
+    
+    if(not qst_old):
+        return '', 404
+    qst_new.index = qst_old.index
+
+    if not Question.saveToDb(qst_new):
+        return '', 500
+    return '', 200
+	
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
